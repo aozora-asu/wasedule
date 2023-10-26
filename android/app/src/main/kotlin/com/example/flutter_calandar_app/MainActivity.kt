ae2bbf8f2c80a530d9de59629aed42187c56a626
TakatsuRyotaro <likelikesaturday@gmail.com>
2023-10-26 14:30:33 +0900

    フロントエンド追加しましたよー

--- android/app/src/main/kotlin/com/example/flutter_calandar_app/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.flutter_calandar_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/frontend/colors.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-
-//ここで使う色を定義します。main.dartで使ってます
-
-const MAIN_COLOR = Color.fromARGB(255, 142, 23, 40);
-const ACCENT_COLOR = Color.fromARGB(255, 215, 196, 28);
