c5e654e4d48e64f84ba6b3cf539b3c4f9b6180a0
TakatsuRyotaro <likelikesaturday@gmail.com>
2023-11-01 21:34:45 +0900

    とりあえずカレンダーページに枠だけ追加。

--- lib/frontend/screens/pages/calendar_page.dart
+++ lib/frontend/screens/pages/calendar_page.dart
@@ -3,10 +3,70 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:table_calendar/table_calendar.dart';
 import 'package:flutter/widgets.dart';
+import 'dart:async';
+import 'package:flutter_calandar_app/backend/DB/db_Manager.dart';
+import 'package:flutter_calandar_app/backend/http_request.dart';
 
+import "../../../backend/temp_file.dart";
 import '../../size_config.dart';
 import '../../colors.dart';
 import 'task_page.dart';
+
+class TaskPage extends StatefulWidget {
+  @override
+  _TaskPageState createState() => _TaskPageState();
+}
+
+class _TaskPageState extends State<TaskPage> {
+  Future<Map<String, dynamic>>? events;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadData();
+  }
+
+  Future<void> _loadData() async {
+    final data = await resisterTaskToDB(url_t);
+    setState(() {
+      events = Future.value(data);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: BACKGROUND_COLOR,
+      body: Center(
+        child: FutureBuilder<Map<String, dynamic>>(
+          future: events,
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return CircularProgressIndicator();
+            } else if (snapshot.hasError) {
+              return Text("Error: ${snapshot.error}");
+            } else if (snapshot.hasData) {
+              // データが読み込まれた場合、リストを生成
+              return buildDataCards(
+                  snapshot.data!["events"] as List<Map<String, dynamic>>);
+            } else {
+              // データがない場合の処理（nullの場合など）
+              return CircularProgressIndicator();
+            }
+          },
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          _loadData();
+        },
+        child: Icon(Icons.get_app), // ボタンのアイコン
+        backgroundColor: MAIN_COLOR, // ボタンの背景色
+      ),
+    );
+  }
+}
+
 
 
 
@@ -94,10 +154,13 @@ class _CalendarState extends State<Calendar> {
   DateTime _focusedDay = DateTime.now();
   DateTime? _selectedDay;
 
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: TableCalendar<dynamic>(
+      body:
+       ListView(children:[
+        TableCalendar<dynamic>(
         firstDay: DateTime.utc(1882, 10, 21),
         lastDay: DateTime.utc(2100, 10, 21),
         focusedDay: _focusedDay,
@@ -167,6 +230,85 @@ class _CalendarState extends State<Calendar> {
           },
         ),
       ),
-    );
-  }
+      SizedBox(
+        height:13,
+      ),
+      TaskList()
+     ]
+   ),
+  );
+ }
+}
+
+class EventData {
+  String dtEnd;
+  String description;
+  String memo;
+
+  EventData(this.dtEnd, this.description, this.memo);
+}
+
+// void main() {
+//   List<EventData> events = [
+//     EventData('2023-10-22 03:59:00.000', '#1 アンケート', null),
+//     EventData('2023-10-23 00:00:00.000', '#2 アンケート', null),
+//     EventData('2023-10-05 05:00:00.000', '質問申請フォーム/Question Application Form', null),
+//   ];
+
+//   for (var event in events) {
+//     print('dtEnd: ${event.dtEnd}, description: ${event.description}, memo: ${event.memo}');
+//   }
+// }
+
+class TaskList extends StatelessWidget {
+  
+ @override
+  Widget build(BuildContext context) {
+    SizeConfig().init(context);
+    return Column(
+      children:[Card(
+       color: WIDGET_COLOR,
+        child: SizedBox(
+         height: SizeConfig.blockSizeHorizontal! * 60,
+         width: SizeConfig.blockSizeHorizontal! * 98,
+         child:Column(
+          children:[
+             Text(
+              ' ～現在の授業課題～',
+              textAlign: TextAlign.left,
+              style: TextStyle(
+                fontSize: SizeConfig.blockSizeHorizontal! * 3.5,
+                fontWeight: FontWeight.w800,
+                color: const Color.fromARGB(255, 77, 46, 35),
+              ),
+            ),
+            Container(
+              height: SizeConfig.blockSizeHorizontal! * 50,
+              width: SizeConfig.blockSizeHorizontal! * 96,
+              decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(8.0),
+              boxShadow: [
+               BoxShadow(
+                  color: Colors.grey,
+                ),
+                BoxShadow(
+                  color: Colors.white,
+                  spreadRadius: -1.5,
+                  blurRadius: 2.0,
+                ),
+              ],                      
+            ),
+            child:ListView(
+              children: [
+                   Text("なななななななななななななななななななななななななななな")
+                ],
+         )
+        )
+       ]
+      )
+     )
+    )
+   ]
+  );
+ }
 }
