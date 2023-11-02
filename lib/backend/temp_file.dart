03c946f1a6f6d1ee7f4afcbcb444a1dc04df76bd
shun <fromage.radix@gmail.com>
2023-11-03 01:46:10 +0900

    一部ファイルをignoreしました

---
+++ lib/backend/temp_file.dart
@@ -0,0 +1,6 @@
+//一時的にテストのためのファイルです。おもに個人情報などをまとめておきます。
+
+String url_y =
+    "https://wsdmoodle.waseda.jp/calendar/export_execute.php?userid=135654&authtoken=9b0e0c9a3d8be79d1ed9211cfd6f1194a40ec330&preset_what=all&preset_time=recentupcoming";
+String url_t =
+    "https://wsdmoodle.waseda.jp/calendar/export_execute.php?userid=140223&authtoken=0c1841efb8714e6edebaeaa5e735d340fcc70f70&preset_what=courses&preset_time=recentupcoming";
