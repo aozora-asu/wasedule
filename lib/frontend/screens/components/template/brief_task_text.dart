899eaf58746d83cc14586bd0d09dea6e4a6ee028
shun <fromage.radix@gmail.com>
2023-11-05 14:13:27 +0900

    簡易タスク表示の年月日を調整し、スクロール可能にしました

--- lib/frontend/screens/components/template/brief_task_text.dart
+++ lib/frontend/screens/components/template/brief_task_text.dart
@@ -3,6 +3,7 @@ import "package:flutter_calandar_app/backend/db_manager.dart";
 import 'package:flutter_calandar_app/frontend/screens/pages/calendar_page.dart';
 import 'package:flutter_calandar_app/frontend/size_config.dart';
 import 'package:flutter_calandar_app/frontend/colors.dart';
+import 'package:intl/intl.dart';
 
 Widget buildTaskText(List<Map<String, dynamic>> taskList) {
   if (taskList.isEmpty) {
@@ -12,7 +13,12 @@ Widget buildTaskText(List<Map<String, dynamic>> taskList) {
   String text = "";
   for (int i = 0; i < taskList.length; i++) {
     text +=
-        '${DateTime.fromMillisecondsSinceEpoch(taskList[i]["dtEnd"]).toString()} ${taskList[i]["title"]} ${taskList[i]["summary"]}';
+        '${DateFormat('yyyy年MM月dd日 HH時mm分').format(DateTime.fromMillisecondsSinceEpoch(taskList[i]["dtEnd"])).toString()} ${taskList[i]["title"]}\n      ${taskList[i]["summary"]}\n';
   }
-  return Text(text);
+  return SingleChildScrollView(
+    child: Text(
+      text,
+      style: const TextStyle(fontSize: 16),
+    ),
+  );
 }
