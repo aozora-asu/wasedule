170beaaf86d8a11c6a330aaf64f57adbc2e45c64
TakatsuRyotaro <148338640+TakatsuRyotaro@users.noreply.github.com>
2024-03-15 14:15:15 +0900

    ヌルっと表示　改良型

--- lib/frontend/screens/common/bottom_bar.dart
+++ lib/frontend/screens/common/bottom_bar.dart
@@ -24,7 +24,7 @@ class CustomBottomBar extends StatelessWidget {
         ),
         BottomNavigationBarItem(
           icon: Icon(Icons.check),
-          label: 'タスク',
+          label: '課題',
           backgroundColor: MAIN_COLOR
         ),
         BottomNavigationBarItem(
