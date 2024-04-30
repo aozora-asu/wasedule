da6e45a44b030cff75771ef468626c5fee7d577d
shun <fromage.radix@gmail.com>
2024-04-30 11:12:52 +0900

    コミット

--- lib/frontend/screens/moodle_view_page/moodle_view_page.dart
+++ lib/frontend/screens/moodle_view_page/moodle_view_page.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_calandar_app/frontend/assist_files/size_config.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
+import 'package:flutter/services.dart' show rootBundle;
+import 'package:url_launcher/url_launcher.dart';
 
 class MoodleViewPage extends ConsumerStatefulWidget {
   @override
@@ -11,55 +13,76 @@ class MoodleViewPage extends ConsumerStatefulWidget {
 class _MoodleViewPageState extends ConsumerState<MoodleViewPage> {
   final GlobalKey webViewKey = GlobalKey();
   InAppWebViewController? webViewController;
-  
+  double progress = 0;
   @override
-  Widget build (BuildContext context){
+  Widget build(BuildContext context) {
     SizeConfig().init(context);
+    String initURL = "https://wsdmoodle.waseda.jp/login/index.php";
     return Scaffold(
-      body: Column(children:[
-        Expanded(
+        body: Column(children: [
+      progress < 1.0
+          ? LinearProgressIndicator(value: progress)
+          : const SizedBox.shrink(),
+      Expanded(
           child: InAppWebView(
-            key: webViewKey,
-            initialUrlRequest: URLRequest(
-              url: WebUri("https://wsdmoodle.waseda.jp/")),
-            onWebViewCreated: (controller) {
-              webViewController = controller;
+        key: webViewKey,
+        onConsoleMessage: (controller, consoleMessage) {
+          print("Value from JavaScript:$consoleMessage");
+        },
+        initialUrlRequest: URLRequest(url: WebUri(initURL)),
+        onWebViewCreated: (controller) {
+          webViewController = controller;
+        },
+        onLoadStop: (controller, url) async {
+          print("Current URL: $url");
+          // JavaScriptファイルの内容を読み込んで実行
+          String javascriptCode = await rootBundle.loadString(
+              'lib/frontend/screens/moodle_view_page/get_course_info.js');
+          await webViewController?.evaluateJavascript(source: javascriptCode);
         },
       )),
-      const Divider(height:0.5,thickness: 0.5, color: Colors.grey),
+      const Divider(height: 0.5, thickness: 0.5, color: Colors.grey),
       Container(
-        color: Colors.white,
-        height:SizeConfig.blockSizeVertical! *4.5,
-        child:menuBar()
-      ),
-      ])
-        
-    );
+          color: Colors.white,
+          height: SizeConfig.blockSizeVertical! * 4.5,
+          child: menuBar()),
+    ]));
   }
 
-  Widget menuBar(){
-    return Row(children:[
-            IconButton(
-                onPressed: () {
-                  webViewController?.goBack();
-                },
-                icon:Icon(Icons.arrow_back_ios,size:SizeConfig.blockSizeVertical! *2.5,),),
-            const Spacer(),
-            IconButton(
-                onPressed: () {
-                  final url = URLRequest(
-                      url: WebUri("https://wsdmoodle.waseda.jp/"));
-                  webViewController?.loadUrl(urlRequest: url);
-                },
-                icon:Icon(Icons.home,size:SizeConfig.blockSizeVertical! *3,),),
-            const Spacer(),
-            IconButton(
-                onPressed: () {
-                  webViewController?.goForward();
-                },
-                icon:Icon(Icons.arrow_forward_ios,size:SizeConfig.blockSizeVertical! *2.5,),),
+  Widget menuBar() {
+    return Row(children: [
+      IconButton(
+        onPressed: () {
+          webViewController?.goBack();
+        },
+        icon: Icon(
+          Icons.arrow_back_ios,
+          size: SizeConfig.blockSizeVertical! * 2.5,
+        ),
+      ),
+      const Spacer(),
+      IconButton(
+        onPressed: () {
+          final url = URLRequest(
+              url: WebUri(
+                  "https://coursereg.waseda.jp/portal/simpleportal.php?HID_P14=JA"));
+          webViewController?.loadUrl(urlRequest: url);
+        },
+        icon: Icon(
+          Icons.home,
+          size: SizeConfig.blockSizeVertical! * 3,
+        ),
+      ),
+      const Spacer(),
+      IconButton(
+        onPressed: () {
+          webViewController?.goForward();
+        },
+        icon: Icon(
+          Icons.arrow_forward_ios,
+          size: SizeConfig.blockSizeVertical! * 2.5,
+        ),
+      ),
     ]);
   }
-
 }
-
