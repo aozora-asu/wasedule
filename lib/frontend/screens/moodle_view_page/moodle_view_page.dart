727bee7162c44f7401acce190590c096b6e75e82
shun <fromage.radix@gmail.com>
2024-05-11 02:13:29 +0900

    学部も検索条件に入れました

--- lib/frontend/screens/moodle_view_page/moodle_view_page.dart
+++ lib/frontend/screens/moodle_view_page/moodle_view_page.dart
@@ -85,6 +85,9 @@ class _MoodleViewPageState extends ConsumerState<MoodleViewPage> {
                       await MyCourseDatabaseHandler()
                           .resisterMyCourse(myCourse);
                     }
+                    // for (var myCourse in myCourseList) {
+                    //   print(myCourse.toMap());
+                    // }
                   }
                 }
             }
