c70505fca68a83da7b1c242543bfd22190f48015
shun <fromage.radix@gmail.com>
2024-05-13 00:33:24 +0900

    設定

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:timezone/data/latest_all.dart' as tz;
 import 'package:timezone/timezone.dart' as tz;
 import "frontend/screens/moodle_view_page/syllabus.dart";
+import "./frontend/screens/moodle_view_page/my_course_db.dart";
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized(); // Bindingの初期化
@@ -27,6 +28,19 @@ void main() async {
   ]);
 
   await initializeDateFormatting(); // 初期化
+  //以下swift連携用の設定
+  // プラットフォームチャンネルの作成
+  const platform = MethodChannel('com.example.flutter_app/channel');
 
+  // 外部から呼び出される関数の実装
+  platform.setMethodCallHandler((call) async {
+    if (call.method == 'getNextCourse') {
+      // 値を取得する処理を行う
+      List<Map<String, dynamic>>? nextCourseList =
+          await MyCourseDatabaseHandler().getNextCourseInfo(DateTime.now());
+      return nextCourseList;
+    }
+    return null;
+  });
   runApp(ProviderScope(child: MyApp()));
 }
