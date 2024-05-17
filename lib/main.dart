d7d6ef27102d81846d71293913ab0b69782bf316
shun <fromage.radix@gmail.com>
2024-05-18 05:15:14 +0900

    調整

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_calandar_app/home_widget.dart';
+import 'package:flutter_calandar_app/backend/home_widget.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'frontend/screens/common/eyecatch_page.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'firebase_options.dart';
+import 'backend/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:timezone/data/latest_all.dart' as tz;
 import 'package:timezone/timezone.dart' as tz;
@@ -20,8 +20,6 @@ void main() async {
   tz.setLocalLocation(tz.getLocation("Asia/Tokyo"));
   await initializeDateFormatting();
   NextCourseHomeWidget().updateNextCourse();
-
-  print(datetime2termList(DateTime(2024, 5, 1)));
 
   runApp(ProviderScope(child: MyApp()));
 }
