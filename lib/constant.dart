38737ed86fe10e45e7c3d02496c059fe5025a6d5
shun <fromage.radix@gmail.com>
2024-07-14 01:06:17 +0900

    出席状況の取得関数の導入

--- lib/constant.dart
+++ lib/constant.dart
@@ -7,3 +7,13 @@ enum Terms {
   fall_semester,
   full_year,
 }
+
+enum AttendStatus {
+  attend('attend'),
+  late('late'),
+  absent('absent'),
+  ;
+
+  const AttendStatus(this.value);
+  final String value;
+}
