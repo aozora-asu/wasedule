abaff6bc36c98e5cdda8bc96ad1e095164a29138
TakatsuRyotaro <ryotakatsu08@gmail.com>
2024-07-29 16:41:58 +0900

    検索UIの実装

--- lib/frontend/screens/timetable_page/syllabus_search_dialog.dart
+++ lib/frontend/screens/timetable_page/syllabus_search_dialog.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_calandar_app/frontend/assist_files/colors.dart';
 import 'package:flutter_calandar_app/frontend/assist_files/size_config.dart';
 import 'package:flutter_calandar_app/frontend/assist_files/ui_components.dart';
 import 'package:flutter_calandar_app/frontend/screens/menu_pages/university_schedule.dart';
 import 'package:flutter_calandar_app/frontend/screens/moodle_view_page/syllabus_query_request.dart';
+import 'package:flutter_calandar_app/frontend/screens/moodle_view_page/syllabus_query_result.dart';
 import 'package:flutter_calandar_app/static/constant.dart';
 
 
@@ -91,7 +93,7 @@ class _SyllabusSearchDialogState extends State<SyllabusSearchDialog> {
             Text(courseTimeText,style: searchConditionTextStyle,),
             const Spacer(),
           ]),
-
+          const Divider(),
           Row(
             children: [
               SizedBox(
@@ -123,6 +125,28 @@ class _SyllabusSearchDialogState extends State<SyllabusSearchDialog> {
                 (value){requestQuery.keyword = value;}),
             ],
           ),
+          const SizedBox(height:5),
+          Row(
+            children: [
+              SizedBox(
+                width: 100,
+                child: Text(
+                  "オープン科目",
+                  style: searchConditionTextStyle,
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              CupertinoCheckbox(
+                value: requestQuery.p_open,
+                onChanged: (value){
+                  setState(() {
+                    requestQuery.p_open = value!;
+                  });
+                })
+            ],
+          ),
+          const Divider(),
+          searchResult()
         ],
       ),
     );
@@ -142,24 +166,8 @@ class _SyllabusSearchDialogState extends State<SyllabusSearchDialog> {
   }
 
   Widget departmentPicker(Department? gakubu) {
-    List<Department?> items = [
-      gakubu,
-      Department.advancedScience,
-      Department.education,
-      Department.internationalEducation,
-      Department.socialScience,
-      Department.commerce,
-      Department.sportsScience,
-      Department.politicalEconomy,
-      Department.fundamentalScience,
-      Department.creativeScience,
-      Department.humanScience,
-      Department.cultureAndMediaStudie,
-      Department.literature,
-      Department.law,
-      Department.global,
-      null
-    ];
+    List<Department?> items = Department.departments;
+    items.insert(0,gakubu);
 
     return Expanded(
       child: CupertinoPicker(
@@ -220,4 +228,43 @@ class _SyllabusSearchDialogState extends State<SyllabusSearchDialog> {
 
     return  value;
   }
+
+  Widget searchResult(){
+    print(requestQuery.p_gakubu!.text);
+    print(requestQuery.p_gakki!.text);
+    print(requestQuery.p_youbi!.text);
+    print(requestQuery.p_jigen!.text);
+    print(requestQuery.keyword);
+    print(requestQuery.p_open);
+    return StreamBuilder(
+      stream: requestQuery.fetchAllSyllabusInfo(),
+      builder: (context,snapShot){
+        if(snapShot.connectionState == ConnectionState.waiting){
+          return const SizedBox(
+            height:70,
+            child:Center(
+              child:CircularProgressIndicator(color:PALE_MAIN_COLOR)));
+        }else if(snapShot.hasData){
+          return ListView.separated(
+            itemBuilder:(context,index){
+              return Text(snapShot.data!.courseName);
+            },
+            separatorBuilder:(context,index){
+              return const SizedBox(height:5);
+            },
+            itemCount: 3,//snapShot.data!.length,
+            shrinkWrap: true,
+            );
+        }else if(snapShot.hasError){
+          return Text(snapShot.error.toString());
+        }else{
+          return const SizedBox(
+            height:70,
+            child:Center(
+              child:Text("検索結果なし",
+                style:TextStyle(color:Colors.grey))));
+        }
+      },);
+  }
+
 }
